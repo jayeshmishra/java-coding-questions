6499dfa9b6b50e86da1edff6a3b2e9e6df310035
Jayesh <jayesh28mishra@gmail.com>
2026-01-09 17:12:12 +0530

    Initial commit - java coding questions

---
+++ src/com/practice/string/CharacterFrequency.java
@@ -0,0 +1,23 @@
+package com.practice.string;
+
+public class CharacterFrequency {
+
+	public static void main(String[] args) {
+
+		String str = "hello";
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			int count = 0;
+
+			for (int j = 0; j < str.length(); j++) {
+				if (str.charAt(j) == c) {
+					count++;
+				}
+			}
+
+			System.out.println(c + "-> " + count);
+		}
+
+	}
+}
---
+++ src/com/practice/string/ReverseString.java
@@ -0,0 +1,18 @@
+package com.practice.string;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str="hello";
+		String rev="";
+		
+	
+		for(int i=str.length()-1; i>=0; i--) {
+			rev+=str.charAt(i);
+		}
+		
+		System.out.println(rev);
+		
+	}
+}
