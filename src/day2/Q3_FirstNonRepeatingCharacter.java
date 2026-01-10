1bc1b7e9d51919e0b34825183535abdc1ac3e4a5
Jayesh <jayesh28mishra@gmail.com>
2026-01-11 01:10:39 +0530

    some changes in day 2 Q3 file

--- src/day2/Q3_FirstNonRepeatingCharacter.java
+++ src/day2/Q3_FirstNonRepeatingCharacter.java
@@ -4,9 +4,30 @@
 //output:w
 
 //brute force logic:
-//Pi
+//1.Pick one character
+//2.Count how many times it appears
+//3.If count==1 -> print and stop
 package day2;
 
 public class Q3_FirstNonRepeatingCharacter {
 
+	public static void main(String[] args) {
+		String str="swiss";
+		
+		for(int i=0; i<str.length(); i++) {
+			char c=str.charAt(i);
+			int count=0;
+			
+			for(int j=0; j<str.length(); j++) {
+				if(str.charAt(j)==c) {
+					count++;
+				}
+			}
+			
+			if(count==1) {
+				System.out.println(c);
+				break;
+			}
+		}
+	}
 }
